9c01150b1ddfa4af5731563e3742b7cbd9f0533e
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-01 15:54:10 +0200

    added lnk page

--- src/app/lnk/lnk.component.ts
+++ src/app/lnk/lnk.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./lnk.component.scss']
 })
 export class LnkComponent {
-
+links = [{
+  title: 'Unsere Wasserperlen bei Amazon.de',
+  url: 'https://www.amazon.de/DekOh-Wasserperlen-Gelperlen-Dekoration-wiederverwendbar/dp/B0C5RHXD8W/ref=sr_1_23?crid=E1C2FN7R7E54&keywords=wasserperlen+f%C3%BCr+pflanzen&qid=1690894707&sprefix=wasserperlen%2Caps%2C113&sr=8-23',
+  pic: 'dekoh_02.jpg'
+},{
+  title: 'DekOh! Website',
+  url: 'https://dekoh.de',
+  pic: 'dekoh_02.jpg'
+}, {
+  title: 'Impressum',
+  url: 'https://',
+  pic: 'dekoh_02.jpg'
+}
+  
+];
 }
