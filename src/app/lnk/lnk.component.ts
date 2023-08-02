56d0503dd851c729ca68d101a5066a97a20f26ac
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-02 17:32:54 +0200

    updated lnk social media

--- src/app/lnk/lnk.component.ts
+++ src/app/lnk/lnk.component.ts
@@ -23,4 +23,14 @@ links = [{
 url: 'https://dekoh.de/privacy',}
   
 ];
+socialMedia = [{
+  icon: 'insta.png',
+  link: 'https://www.instagram.com/dekoh.de/'
+},{
+  icon: 'fb.png',
+  link: 'https://www.facebook.com/profile.php?id=100095104699877'
+},{
+  icon: 'mail.png',
+  link: 'mailto:mail@hdekoh.de'
+}]
 }
