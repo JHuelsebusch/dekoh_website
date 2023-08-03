7848a80899905b1239a3b04e72d5fd59e9158375
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-03 19:41:23 +0200

    added follow us images

--- src/app/follow-us/follow-us.component.ts
+++ src/app/follow-us/follow-us.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocialmediaService } from '../socialmedia.service';
 
 @Component({
   selector: 'app-follow-us',
   templateUrl: './follow-us.component.html',
   styleUrls: ['./follow-us.component.scss']
 })
-export class FollowUsComponent {
+export class FollowUsComponent implements OnInit {
+  constructor(public _sms:SocialmediaService){}
+  socialMedia:any= [];
+  images: string[] = ['beads_1.png','beads_2.png','beads_3.png','beads_4.png','beads_5.png',];
+currentImage:number = 1;
+image:string ='beads_1.png';
+
+ngOnInit(): void {
+  this.socialMedia = this._sms.socialMedia;
+  this.deleteMailOnSocialMediaArray();
+  this.changeImage();
+}
+deleteMailOnSocialMediaArray(){
+let index = this.socialMedia.findIndex((obj: { icon: string; }) =>obj.icon == 'mail.png');
+this.socialMedia.splice(index,1);
+}
+changeImage(){
+  setInterval(() => {this.count();}, 5000)
+}
+count(){
+  // if(this.c)
+  this.currentImage = this.currentImage+1;
+  console.log(this.currentImage)
+  this.image = this.images[this.currentImage];
+}
 
 }
--- src/app/lnk/lnk.component.ts
+++ src/app/lnk/lnk.component.ts
@@ -8,7 +8,7 @@ import { SocialmediaService } from '../socialmedia.service';
 })
 export class LnkComponent {
   constructor(public _sms: SocialmediaService) {}
-socialMedia =[];
+
 links = [{
   title: 'Unsere Wasserperlen bei Amazon.de',
   url: 'https://www.amazon.de/DekOh-Wasserperlen-Gelperlen-Dekoration-wiederverwendbar/dp/B0C5RHXD8W/ref=sr_1_23?crid=E1C2FN7R7E54&keywords=wasserperlen+f%C3%BCr+pflanzen&qid=1690894707&sprefix=wasserperlen%2Caps%2C113&sr=8-23',
