45c02e69ca9a5e7aea58057c6657ecd31a75ece3
JHuelsebusch <jhuelsebusch@gmx.de>
2023-08-07 16:24:35 +0200

    updated responsive view

--- src/app/shop-url.service.ts
+++ src/app/shop-url.service.ts
@@ -7,9 +7,11 @@ export class ShopUrlService {
 
   constructor() { }
   
+  amzLink: string = 'https://www.amazon.de/DekOh-Wasserperlen-Gelperlen-Dekoration-wiederverwendbar/dp/B0C5RHXD8W/ref=sr_1_16?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2GLG76FVUPXAB&keywords=wasserperlen+deko&qid=1691416473&sprefix=wasserperlen+deko%2Caps%2C111&sr=8-16'
+
   navigateTo(url: string){
     if(url=='amz'){
-      window.open('https://www.amazon.de/DekOh-Wasserperlen-Gelperlen-Dekoration-wiederverwendbar/dp/B0C5RHXD8W/ref=sr_1_23?crid=E1C2FN7R7E54&keywords=wasserperlen+f%C3%BCr+pflanzen&qid=1690894707&sprefix=wasserperlen%2Caps%2C113&sr=8-23');
+      window.open(this.amzLink);
     }
   }
 }
